5276109fa63d990c175540d371dff12c48a4ac12
Krul <vincent.krul@sogeti.com>
2020-01-19 21:34:25 +0100

    httpclient voorbeeld

--- src/app/heroes/hero.ts
+++ src/app/heroes/hero.ts
@@ -1,7 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
+export interface Hero {
+  id: number;
+  name: string;
+}
 
 
 /*
